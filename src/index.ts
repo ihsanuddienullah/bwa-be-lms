574197a15d5eb68e4c2b430f879614fd87beb58a
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-08-20 14:19:02 +0700

    add and setup additional packages

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { config } from 'dotenv'
 import express from 'express'
 
 const app = express()
 const port = 3000
+
+config()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
