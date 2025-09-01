9e0e6e91216ad0987f6b95db35ecef47b5e0e778
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-09-01 20:09:59 +0700

    update sign up schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -5,7 +5,7 @@ export const exampleSchema = z.object({
 })
 
 export const signUpSchema = z.object({
-  name: z.string().min(5, 'Name is required'),
-  email: z.email().min(5),
-  password: z.string().min(5, 'Password is required'),
+  name: z.string().min(1, 'Name is required'),
+  email: z.email('Invalid email address'),
+  password: z.string().min(5, 'Password must be at least 5 characters long'),
 })
